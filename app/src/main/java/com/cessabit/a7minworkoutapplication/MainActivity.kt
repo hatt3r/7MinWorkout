8d30e8626f90aac0834e0a12b0c8ed145a389c9a
Serene <serene719@gmail.com>
2023-12-05 10:52:18 +0530

    📈 setting up first UI

--- app/src/main/java/com/cessabit/a7minworkoutapplication/MainActivity.kt
+++ app/src/main/java/com/cessabit/a7minworkoutapplication/MainActivity.kt
@@ -2,10 +2,21 @@ package com.cessabit.a7minworkoutapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartButton: FrameLayout = findViewById(R.id.flStart)
+        flStartButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                "here we will start ",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
